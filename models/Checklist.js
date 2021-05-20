5bbc498c2988d6b9fc9e044cc07412e946a45a15
Blaze Lim <blazebentleycolim@gmail.com>
2021-05-20 14:45:36 -0700

    added models

---
+++ models/Checklist.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class Checklist extends Model {}
+
+Checklist.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      checklist_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      },
+      user_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+              model: 'user',
+              key: 'id'
+          }
+      }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'checklist'
+    }
+);
+
+module.exports = Checklist;
